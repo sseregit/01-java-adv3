99277a5739be32c1224b5d061a62349cc73fe842
장연식 <sseregit@knowwheresoft.com>
2025-05-12 15:20:17 +0900

    문제와 풀이2

---
+++ src/lambda/ex2/MapExample.java
@@ -0,0 +1,30 @@
+package lambda.ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapExample {
+    // 고차 함수, 함수를 인자로 받아, 리스트의 각 요소를 변환
+    public static List<String> map(List<String> list, StringFunction func) {
+        List<String> results = new ArrayList<>();
+
+        for (String word : list) {
+            results.add(func.apply(word));
+        }
+        // 코드 작성
+        return results; // 제거하고 적절한 객체를 반환
+    }
+
+    public static void main(String[] args) {
+        List<String> words = List.of("hello", "java", "lambda");
+        System.out.println("원본 리스트: " + words);
+        // 1. 대문자 변환
+        // 코드 작성
+        List<String> uppercaseWords = map(words, word -> word.toUpperCase());
+        System.out.println("대문자 변환 결과: " + uppercaseWords);
+        // 2. 앞뒤에 *** 붙이기 (람다로 작성)
+        // 코드 작성
+        List<String> decorateWords = map(words, word -> "***" + word + "***");
+        System.out.println("특수문자 데코 결과: " + decorateWords);
+    }
+}
